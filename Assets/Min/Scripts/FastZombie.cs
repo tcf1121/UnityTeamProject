8a67c0db38d2c2f510732ca93a83c44246e77809
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-28 17:30:59 +0900

    [Feat] FastZombie 자식 클래스 구현
    상속 및 프로퍼티 수정을 통한 FastZombie 자식 클래스 구현

---
+++ Assets/Min/Scripts/FastZombie.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastZombie : Zombie
+{
+    protected override string Name { get; set; } = "FastZombie";
+    protected override int CurrentHealth { get; set; } = 5;
+    protected override int MaxHealth { get; set; } = 5;
+    protected override int Power { get; set; } = 2;
+    protected override float AttackSpeed { get; set; } = 2f;
+    protected override float MoveSpeed { get; set; } = 2f;
+    protected override int Level { get; set; } = 1;
+    protected override int DropGold { get; set; } = 1;
+    protected override int DropExp { get; set; } = 1;
+    protected override float AttackRange { get; set; } = 1f;
+}
+
