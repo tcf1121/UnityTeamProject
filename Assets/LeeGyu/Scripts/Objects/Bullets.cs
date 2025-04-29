eab20750421f27fbb7b2dfbd4159abb7cd6f3727
LGY925 <olo66499133@gmail.com>
2025-04-29 09:56:09 +0900

    [feat]bullets  부모클레스 구현

---
+++ Assets/LeeGyu/Scripts/Objects/Bullets.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Bullets : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] Rigidbody rigid;
+    [SerializeField] private float returnTime;
+    private float timer;
+
+
+    [Header("Property")]
+    [SerializeField] private float attackPower;
+    [SerializeField] protected float speed;
+    public float AttackPower
+    { get { return attackPower; } set { attackPower = value; } }
+
+    private void OnEnable()
+    {
+        timer = returnTime;
+    }
+    private void Update()
+    {
+        Shot();
+    }
+
+    protected void OnCollisionEnter(Collision collision)
+    {
+        IDamgable damgable = collision.gameObject.GetComponent<IDamgable>();
+        if (damgable != null)
+        {
+            Attack(damgable);
+        }
+        Destroy(gameObject);
+    }
+
+    private void Shot()
+    {
+        AttackMethod();
+        timer -= Time.time;
+        if (timer < 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    protected abstract void AttackMethod();
+
+    public void Attack(IDamgable damgable)
+    {
+        damgable.TakeDamage(gameObject, attackPower);
+    }
+
+}
