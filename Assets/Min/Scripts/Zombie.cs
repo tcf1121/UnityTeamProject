eae4ceecbcc43f4721abe816f4f8b8546d252674
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-05-02 14:15:40 +0900

    [style] Zombie 클래스 코드 정렬

--- Assets/Min/Scripts/Zombie.cs
+++ Assets/Min/Scripts/Zombie.cs
@@ -13,27 +13,27 @@ public abstract class Zombie : MonoBehaviour
     // 바꿔야될 클래스
     //private TestPlant targetPlant;
 
-    [SerializeField] protected int currentHealth { get; set; }
+    [SerializeField] protected int currentHealth;
     public int CurrentHealth => currentHealth;
 
 
-    [SerializeField] protected int maxHealth { get; set; }
+    [SerializeField] protected int maxHealth;
     public int MaxHealth => maxHealth;
 
-    [SerializeField] protected  int power { get; set; }
+    [SerializeField] protected int power;
     public int Power => power;
 
-    [SerializeField] protected float attackSpeed { get; set; }
-    public float AttackSpeed => attackSpeed;    
+    [SerializeField] protected float attackSpeed;
+    public float AttackSpeed => attackSpeed;
 
-    [SerializeField] protected float moveSpeed { get; set; }
+    [SerializeField] protected float moveSpeed;
     public float MoveSpeed => moveSpeed;
 
 
-    [SerializeField] protected int level { get; set; }
+    [SerializeField] protected int level;
     public int Level => level;
     
-    [SerializeField] protected int dropGold { get; set; }
+    [SerializeField] protected int dropGold;
     public int DropGold => dropGold;
 
 
@@ -41,6 +41,11 @@ public abstract class Zombie : MonoBehaviour
     public static event Action<int> OnZombieDied; // 좀비가 죽을 경우에 이벤트 발생
     public int goldReward = 1;
     [SerializeField] GameObject goldDropEffectPrefab;
+
+    protected virtual void Awake()
+    {
+  
+    }
 
 
     protected virtual void Update()
