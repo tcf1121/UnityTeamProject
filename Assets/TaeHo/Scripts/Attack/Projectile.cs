8f62391769a318e276af2a8f8d6764de7913b4e6
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-05-02 17:34:06 +0900

    [feat] 투사체 로직 생성

---
+++ Assets/TaeHo/Scripts/Attack/Projectile.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // 투사체
+    public float projectileSpeed = 1f; // 투사체 속도
+
+    public int projectileDamage;  // 투사체 데미지
+
+    private GameObject shooter;  // 쏘는 캐릭터 붙이면 됨
+
+
+    public void Initialize(int damage, GameObject shooter)
+    {
+        this.projectileDamage = damage;
+        this.shooter = shooter;
+    }
+
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       ITarget target = other.GetComponent<ITarget>();
+        
+        if (target != null)
+        {
+            target.TakeDamage(projectileDamage);
+            Destroy(gameObject);
+        }
+
+    }
+}
