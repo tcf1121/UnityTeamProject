49479adac6017181ef4134a5b50e47379aa90460
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-02 15:30:08 +0900

    [Test] cell to world 좌표 테스트 코드 추가
    cell to world 좌표 테스트 코드 추가

--- Assets/Min/Scripts/HexCellDebuggger.cs
+++ Assets/Min/Scripts/HexCellDebuggger.cs
@@ -8,15 +8,15 @@ public class HexCellDebuggger : MonoBehaviour
     [SerializeField] Tilemap tilemap;
 
     public Vector2Int currentCellPos;
-
+    public Vector3 currentWorldCenter;
     private void Update()
     {
         if (tilemap == null) return;
 
         Vector3 worldPos = transform.position;
-
         Vector3Int cellPos = tilemap.WorldToCell(worldPos);
-
         currentCellPos = new Vector2Int(cellPos.x, cellPos.y);
+
+        currentWorldCenter = tilemap.GetCellCenterWorld(cellPos);
     }
 }
