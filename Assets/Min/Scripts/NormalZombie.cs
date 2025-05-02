6717260826c2c8344368b6758659dddbbec6ef2d
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-05-02 14:17:34 +0900

    [feat] NormalZombie 생성
    일반 유닛 생성
    특별한 기능은 없음

--- Assets/Min/Scripts/NormalZombie.cs
+++ Assets/Min/Scripts/NormalZombie.cs
@@ -5,6 +5,34 @@ using UnityEngine;
 
 public class NormalZombie : Zombie
 {
-    
+    private void Start()
+    {
+        maxHealth = 100;
+        currentHealth = maxHealth;
+
+        Debug.Log($"현재 체력은 {currentHealth}입니다.");
+
+    }
+
+
+    protected override void Update()
+    {
+        base.Move(); // 몬스터의 움직임이나 범위 내에 있다면 공격하는 부분까지 구현함
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (CurrentHealth < 0)
+        {
+            Die();
+        }
+    }
+
+    public override void Die()
+    {
+        base.Die();
+    }
 
 }
