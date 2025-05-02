fcc795910ee4a8cccf26b969b2f5e2c62f528b60
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-05-02 17:32:20 +0900

    [feat] 원거리 공격 로직 작성

---
+++ Assets/TaeHo/Scripts/Attack/RangedAttack.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedAttack : AttackBase
+{
+    // 원거리 공격
+
+    public GameObject projectilePrefab;  // 투사체 프리팹 (활, 총알 인지 모르니 투사체로 정의)
+    public Transform firePoint;  // 투사체가 나갈 방향
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+
+    protected override void Attack()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+        projectile.GetComponent<Projectile>().Initialize(damage, gameObject);
+
+    }
+}
