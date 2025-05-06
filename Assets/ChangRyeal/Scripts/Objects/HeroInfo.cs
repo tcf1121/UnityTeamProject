d68409e8720d346c6f33fccc7b5661cb39c9d48c
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-06 15:08:53 +0900

    [Feat] 상태 표시창 로직 수정
    상태 표시창 로직 수정

--- Assets/ChangRyeal/Scripts/Objects/HeroInfo.cs
+++ Assets/ChangRyeal/Scripts/Objects/HeroInfo.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using YongSeok;
@@ -7,13 +9,25 @@ using YongSeok;
 public class HeroInfo : MonoBehaviour
 {
     [SerializeField] public GameObject hero;
+    [SerializeField] HeroStatus_ heroStatus;
 
     [Header("UI")]
     [SerializeField] private Image heroImage;
     [SerializeField] private Button sellBtn;
+    [SerializeField] private TMP_Text statsText;
+
+
+    private void Awake()
+    {
+        if (statsText == null)
+            statsText = transform.Find("Text (TMP)").GetComponent<TMP_Text>();
+    }
+
     public void SetInfo()
     {
         heroImage.sprite = hero.GetComponent<Hero>().sprite;
+        heroStatus = hero.GetComponent<HeroStatus_>();
+        statsText.text = heroStatus.GetStatus();
     }
 
     private void OnEnable()
