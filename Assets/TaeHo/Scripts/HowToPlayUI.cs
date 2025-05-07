af7192d47ddd13174e30e4969490d03dea951d53
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-05-07 16:26:35 +0900

    [chore] UI 수정

--- Assets/TaeHo/Scripts/HowToPlayUI.cs
+++ Assets/TaeHo/Scripts/HowToPlayUI.cs
@@ -11,4 +11,13 @@ public class HowToPlayUI : MonoBehaviour
         howToPlayPanel.SetActive(!howToPlayPanel.activeSelf);
     }
 
+    public void Update()
+    {
+        // ESC 눌렀을 때 설명창이 켜져 있으면 닫기
+        if (howToPlayPanel.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+        {
+            howToPlayPanel.SetActive(false);
+        }
+    }
+
 }
